13ef6707413574e9c287bbe131c5a57bf06dfd93
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2018-07-05 19:12:54 +0300

    unimportant

--- code_app.lua
+++ code_app.lua
@@ -4,8 +4,6 @@ local glue = require'glue'
 
 local tablist = ui.tablist:subclass'ce_tablist'
 
-tablist.main_tablist = false
-
 local tab = ui.tab:subclass'ce_tab'
 
 --tab.focusable = false
@@ -19,7 +17,7 @@ editbox.editor = {
 	line_numbers = true,
 }
 
-ui:style('ce_editbox :focused', {
+ui:style('ce_editbox, ce_editbox :focused', {
 	shadow_blur = 0,
 	background_color = false,
 })
@@ -27,16 +25,44 @@ ui:style('ce_editbox :focused', {
 local win = ui:window{
 	w = 800,
 	h = 500,
-	maximized = true,
+	min_cw = 300,
+	min_ch = 200,
+	--maximized = true,
+	frame = false,
+	view = {
+		border_width = 1,
+		border_color = '#111',
+		padding = 6,
+		padding_right = 12,
+	},
 }
 
 local tabs = tablist(win, {
-	w = win.cw,
+	tab_slant_left = 80,
+	tab_slant_right = 75,
 })
+
+win.move_layer = tabs
+
+tabs.max_click_chain = 2
+function tabs:doubleclick()
+	if win.ismaximized then
+		win:restore()
+	else
+		win:maximize()
+	end
+end
+
+local frame = ui:layer{
+	parent = win,
+	border_color = '#222',
+	border_width = 1,
+	corner_radius = 5,
+}
 
 function tabs:add_tab(file)
 
-	local editbox = editbox(win, {
+	local editbox = editbox(frame, {
 		text = assert(glue.readfile(file)),
 		visible = false,
 	})
@@ -60,11 +86,15 @@ function tab:tab_unselected()
 end
 
 function tabs:after_sync()
+	tabs.w = win.view.cw
+	frame.x = tabs.x
+	frame.y = tabs.y2
+	frame.w = tabs.w
+	frame.h = win.view.ch - tabs.h
 	for i,tab in ipairs(self.tabs) do
 		local e = tab.editbox
-		e.w = win.cw
-		e.h = win.ch - self.h
-		e.y = win.ch - e.h
+		e.w = tabs.w
+		e.h = win.view.ch - tabs.h
 	end
 end
 
@@ -75,6 +105,12 @@ end
 
 local t1 = tabs:add_tab('code_app.lua')
 local t2 = tabs:add_tab('codedit.lua')
+local t3 = tabs:add_tab('ui.lua')
+
+function win:client_rect_changed(cx, cy, cw, ch)
+	tabs:sync()
+end
+
 
 ui:run()
 
